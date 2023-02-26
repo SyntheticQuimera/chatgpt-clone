9177b16316a93720c0dfa7ee21507fb66b02961b
syntheticquimera <syntheticquimera@gmail.com>
2023-02-26 13:55:18 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,39 @@
-import './globals.css'
+import "./globals.css";
+import SessionProvider from "./components/SessionProvider";
+import SideBar from "./components/SideBar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import Login from "./components/Login";
+import ClientProvider from "./components/ClientProvider";
+import Header from "./components/Header";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang='en'>
       <head />
-      <body>{children}</body>
+      <body className=''>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className='flex text-textColor'>
+              <div className='hidden md:inline'>
+                <SideBar />
+              </div>
+              <Header />
+              <ClientProvider />
+              <div className='flex-1 overflow-y-auto bg-background scrollbar-thin scrollbar-track-background scrollbar-thumb-scrollbarThumb hover:scrollbar-thumb-scrollbarThumbHover '>
+                {children}
+              </div>
+            </div>
+          )}
+        </SessionProvider>
+      </body>
     </html>
-  )
+  );
 }
